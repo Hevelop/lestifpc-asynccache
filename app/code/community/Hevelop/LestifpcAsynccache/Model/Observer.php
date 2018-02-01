3fa4f61d09a7bc6626d254fdef57e2899b79d0d7
Alessandro Pagnin <alepane@gmail.com>
2018-02-01 17:31:33 +0100

    add management of redis server

--- app/code/community/Hevelop/LestifpcAsynccache/Model/Observer.php
+++ app/code/community/Hevelop/LestifpcAsynccache/Model/Observer.php
@@ -2,10 +2,70 @@
 
 class Hevelop_LestifpcAsynccache_Model_Observer
 {
+    const CACHE_TYPE_CONFIG = 'config';
+    const CACHE_TYPE_FPC = 'fpc';
+    const CONFIG_CACHE_SERVICES_ROOT = 'redismanager/asynccache/';
+    protected $_allBackends = [
+        'fpc' => null,
+        'config' => null
+    ];
+
+    public function getAllowedConfigType()
+    {
+        return [self::CACHE_TYPE_CONFIG, self::CACHE_TYPE_FPC];
+    }
+
+    /**
+     * Returns the names of the services selected by backend
+     * @param $cacheType
+     * @return array
+     */
+    public function getRedisServicesSelected($cacheType)
+    {
+        return explode('::', Mage::getStoreConfig(self::CONFIG_CACHE_SERVICES_ROOT . $cacheType));
+    }
+
+    public function getRedisServices($cacheType)
+    {
+        if (!in_array($cacheType, $this->getAllowedConfigType())) {
+            $cacheType = self::CACHE_TYPE_CONFIG;
+        }
+
+        if (!is_null($this->_allBackends[$cacheType])) {
+            return $this->_allBackends[$cacheType];
+        }
+
+        $this->_allBackends[$cacheType] = [];
+
+        /** @var Steverobbins_Redismanager_Helper_Data $_helper */
+        $_helper = Mage::helper('redismanager');
+        if (!$_helper) {
+            return $this->_allBackends;
+        }
+        $servicesData = $_helper->getServices();
+        foreach ($servicesData as $serviceData) {
+            $cacheConfigName = $cacheType == 'config' ? 'cache' : $cacheType;
+            $service = new Mage_Core_Model_Cache([
+                'id_prefix' => (string) Mage::getConfig()->getNode('global/' . $cacheConfigName . '/prefix'),
+                'backend' => (string) Mage::getConfig()->getNode('global/' . $cacheConfigName . '/backend'),
+                'backend_options' => [
+                    'port' => $serviceData['port'],
+                    'database' => $serviceData['db'],
+                    'password' => $serviceData['password'],
+                    'server' => $serviceData['host'],
+                ]
+            ]);
+            if (in_array($serviceData['name'], $this->getRedisServicesSelected($cacheType))) {
+                $this->_allBackends[$cacheType][] = $service;
+            }
+        }
+        return $this->_allBackends[$cacheType];
+    }
+
     public static function convertCacheTagToLesti($original_tag)
     {
         $tag = false;
-        if (strpos($original_tag, 'cms') !== false) {
+        if (strpos($original_tag, 'cms_page_') !== false) {
             $tag = sha1(str_replace('cms_page', 'cms', $original_tag));
         } else if (strpos($original_tag, 'catalog_product_') !== false) {
             $tag = sha1(str_replace('catalog_product', 'product', $original_tag));
@@ -30,9 +90,9 @@ class Hevelop_LestifpcAsynccache_Model_Observer
                     $tags_chunked = array_chunk($tags, 5);
                     foreach ($tags_chunked as $tags_chunk) {
                         $asyncCache->setTstamp(time())
-                            ->setMode('matchingAnyTag')
+                            ->setMode(Zend_Cache::CLEANING_MODE_MATCHING_ANY_TAG)
                             ->setTags(implode(',', $tags_chunk))
-                            ->setCacheType('fpc')
+                            ->setCacheType(self::CACHE_TYPE_FPC)
                             ->setStatus(Aoe_AsyncCache_Model_Asynccache::STATUS_PENDING);
                         try {
                             $asyncCache->save();
@@ -41,6 +101,42 @@ class Hevelop_LestifpcAsynccache_Model_Observer
                             // Table might not be created yet. Just go on without returning...
                         }
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * If thereis redismanager, it also send the tags to all the cache instances.
+     *
+     * @param Mage_Core_Model_Observer|Varien_Event_Observer $observer
+     */
+    public function postProcessJobCollection(Varien_Event_Observer $observer)
+    {
+        /** @var $jobCollection Aoe_AsyncCache_Model_JobCollection */
+        $jobCollection = $observer->getData('jobCollection');
+        if (!$jobCollection) {
+            return;
+        }
+        foreach ($jobCollection as $job) {
+            /** @var $job Aoe_AsyncCache_Model_Job */
+            if (in_array($job->getCacheType(), $this->getAllowedConfigType())) {
+                try {
+                    $tags = $job->getTags();
+                    $services = $this->getRedisServices($job->getCacheType());
+                    if (!empty($services)) {
+                        foreach ($services as $service) {
+                            /** @var $service Mage_Core_Model_Cache */
+                            $service->clean($tags);
+                        }
+                        Mage::log(sprintf('[LESTIFPC-ASYNCCACHE] MODE: %s, DURATION: %s sec, TAGS: %s',
+                            $job->getMode(),
+                            $job->getDuration(),
+                            implode(',', $tags)
+                        ));
+                    }
+                } catch (Exception $e) {
+                    Mage::log(sprintf('[LESTIFPC-ASYNCCACHE] ERROR: %s, TAGS: %s', $e->getMessage(), implode(',', $tags)));
                 }
             }
         }
